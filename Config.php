64642f28eef82386f5cdb1ca49b650b76ebf6549
SayakaIto <asenakiriu@Sayaka-no-MacBook-Air.local>
2014-10-16 01:06:24 +0900

    ベースクラスから作ってみた

---
+++ Config.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asenakiriu
+ * Date: 2014/10/16
+ * Time: 0:24
+ */
+
+class Config {
+
+    protected $_api_token = '99c49762b66ddc1005e06168c25f6337';
+
+    protected $_api_base_url = 'https://api.chatwork.com/';
+
+    protected $_api_version = 'v1';
+
+
+    /**
+     * get api_token
+     * @return string
+     */
+    public function getApiToken() {
+        return $this->_api_token;
+    }
+
+    public function get_api_base_url() {
+        return $this->_api_base_url;
+    }
+
+    public function gt_end_point_url() {
+        return $this->_api_base_url.'/'.$this->_api_version;
+    }
+
+} 
---
+++ lib/chatwork/ApiBase.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: asenakiriu
+ * Date: 2014/05/10
+ * Time: 18:24
+ */
+
+namespace chatwork;
+
+
+class ApiBase {
+
+    public function __construct() {
+
+    }
+
+    /**
+     * api request method
+     */
+    public function request() {
+
+    }
+
+}
+
